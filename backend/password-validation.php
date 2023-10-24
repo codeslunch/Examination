54d5ba7e399f2a8b0ee1289c4e071e959f0b0377
Codeslunch <kvnghussy@gmail.com>
2023-10-24 08:58:40 +0100

    Edited my password validation code

--- backend/password-validation.php
+++ backend/password-validation.php
@@ -11,11 +11,11 @@ $run = "SELECT * from users WHERE email = '$email'";
 $runCode = $conn->prepare($run);
 $runCode->execute();
 $count = $runCode->rowCount();
-$account = $runCode->fetch(PDO::FETCH_OBJ);
-$hashedPassword = $account->passkey;
 $accountExists = $count > 0;
 
 if($accountExists) {
+    $account = $runCode->fetch(PDO::FETCH_OBJ);
+    $hashedPassword = $account->passkey;
     if(password_verify($password, $hashedPassword)) {
         echo "Login Succesful";
     } else {
