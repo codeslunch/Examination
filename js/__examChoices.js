89d1e3a9c2d5b643710516c35fa4e482a8b7075b
Codeslunch <kvnghussy@gmail.com>
2023-10-30 02:45:08 +0100

    A little bit of hardwork

---
+++ js/__examChoices.js
@@ -0,0 +1,4 @@
+const examInfo = document.querySelector("#exam");
+examInfo.addEventListener("submit", (event) => {
+	event.preventDefault();
+});
--- js/extension.js
+++ js/extension.js
@@ -1,21 +1,56 @@
+const show__options = document.querySelector("#options");
+const prev = document.querySelector("#backward");
+const next = document.querySelector("#forward");
+const demo = document.querySelector("#demo");
+let key;
+
+const random__code = function () {
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+	const code = [];
+	for (let a = 0; a < 6; a++) {
+		code.push(chars[Math.floor(Math.random() * chars.length)]);
+	}
+	return code.join("");
+};
+
+const myCode = random__code();
+
 const load__exam = async function () {
 	const handler = await fetch("./json/rand.json");
 	const response = await handler.json();
 	return response;
 };
 const display__exam = async function () {
-	const prev = document.querySelector("#backward");
-	const next = document.querySelector("#forward");
-	const demo = document.querySelector("#demo");
-	let key;
 	const handler = await load__exam();
 	key = 0;
 	demo.innerText = handler[key].question;
+	const display__options = function () {
+		const wrapper = document.createElement("ul");
+		let label, input, text;
+		let i = 0;
+		const options = handler[key].options;
+		const len = options.length;
+		while (i < len) {
+			label = document.createElement("label");
+			input = document.createElement("input");
+			text = document.createTextNode(`${options[i]}`);
+			input.setAttribute("type", "radio");
+			input.setAttribute("name", myCode);
+			label.appendChild(input);
+			label.appendChild(text);
+			wrapper.appendChild(label);
+			show__options.appendChild(wrapper);
+			i++;
+		}
+	};
+	display__options();
 
 	prev.addEventListener("click", function () {
 		if (key > 0) {
 			key -= 1;
 			demo.innerText = handler[key].question;
+			show__options.innerHTML = "";
+			display__options();
 		}
 	});
 
@@ -24,6 +59,8 @@ const display__exam = async function () {
 		if (key < totalQuestionsCount) {
 			key += 1;
 			demo.innerText = handler[key].question;
+			show__options.innerHTML = "";
+			display__options();
 		}
 	});
 };
